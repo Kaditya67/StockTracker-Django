2b749cb00d6849f61218e542ca025df69bbc998b
Kaditya67 <322adityakumar0004@dbit.in>
2024-01-20 10:32:49 +0530

    Index Page Added

--- StockTracker/Stocks/views.py
+++ StockTracker/Stocks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.contrib import messages
+
 
 # Create your views here.
+def index(request):
+    # messages.success(request,"This is the test message")
+    return render(request,'templates\index.html')
